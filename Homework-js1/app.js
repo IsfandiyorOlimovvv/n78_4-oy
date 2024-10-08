b8f1c543a366f3c261ef658f34c87f269d55ec03
IsfandiyorOlimovvv <shoxagaming>
2024-10-08 17:18:53 +0500

    js

---
+++ Homework-js1/app.js
@@ -0,0 +1,47 @@
+// let names = prompt("what is your full name");
+// let age = prompt("how old are you");
+// let grade = prompt("what is your grade status");
+// let subjects = prompt("what is your favourite subjects");
+// let grades = prompt("what is your grades");
+// let averageGrade = prompt("what is your averageGrade");
+// let highestGrade = prompt("what is your highestGrade");
+// let lowestGrade = prompt("what is your lowestGrade");
+// let address = prompt("where is your address");
+// let phonenumber = prompt("what is your phone number");
+// let email = prompt("what is your email");
+// let courses = prompt("what is your course");
+// let city = prompt("what is your city");
+// let isMarried = prompt("Are you married");
+// let hobbies = prompt("what is your hobbie");
+// let familyStatus = prompt("How much is your family status");
+// let familyMembers = prompt("How much is your family members");
+// let siblings = prompt("what is your sibling");
+// let studentoff = prompt("what is student off");
+
+// console.log(names);
+// console.log(age);
+// console.log(grade);
+// console.log(subjects);
+
+//2
+
+// let names = prompt ("what is your name");
+// console.log(names);
+// let age = prompt ("how old are you");
+// console.log(age);
+// let city = prompt ("what is your city");
+// console.log(city);
+// let address = prompt ("what is your address");
+// console.log(address);
+// let phonenumber = prompt ("what is your phone number");
+// console.log(phonenumber);
+// let course = prompt ("what is your course");
+// console.log(course);
+// let coursenumber = prompt ("what is your course number");
+// console.log(coursenumber);
+// let subject = prompt ("what is your favourite subject");
+// console.log(subject);
+// let familyMembers = prompt ("how many your family members");
+// console.log(familyMembers);
+// let hobbies = prompt ("what is your hobbie");
+// console.log(hobbies);
---
+++ lesson-1/app.js
@@ -0,0 +1,24 @@
+let names = prompt("what is your full name");
+let age = prompt("how old are you");
+let grade = prompt("what is your grade status");
+let subjects = prompt("what is your favourite subjects");
+let grades = prompt("what is your grades");
+let averageGrade = prompt("what is your averageGrade");
+let highestGrade = prompt("what is your highestGrade");
+let lowestGrade = prompt("what is your lowestGrade");
+let address = prompt("where is your address");
+let phonenumber = prompt("what is your phone number");
+let email = prompt("what is your email");
+let courses = prompt("what is your course");
+let city = prompt("what is your city");
+let isMarried = prompt("Are you married");
+let hobbies = prompt("what is your hobbie");
+let familyStatus = prompt("How much is your family status");
+let familyMembers = prompt("How much is your family members");
+let siblings = prompt("what is your sibling");
+let studentoff = prompt("what is student off");
+
+console.log(names);
+console.log(age);
+console.log(grade);
+console.log(subjects);
